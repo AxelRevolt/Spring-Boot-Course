4669fad791fccab8a621aed8998f9b9ca41c23cd
AxelRevolt <depasquale993@gmail.com>
2024-03-19 15:51:43 +0100

    component-scanning

---
+++ 02-srpingboot-spring-core/02-componet-scanning/src/main/java/com/spring/coredemo/controller/Controller.java
@@ -0,0 +1,29 @@
+package com.spring.coredemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.coredemo.interfacetest.Coach;
+
+@RestController
+public class Controller {
+	
+	private Coach myCoach;// = new Test(); the interface can inizializate class that implements it
+	
+	@Autowired
+	public Controller(Coach myCoach) {
+		this.myCoach = myCoach;
+	}
+	
+	@GetMapping("/getDalyWorkout")
+	public String getDalyWorkout() {
+		return myCoach.getDalyWorkout();
+	}
+	
+	@GetMapping("/")
+	public String test() {
+		return "test";
+	}
+
+}
