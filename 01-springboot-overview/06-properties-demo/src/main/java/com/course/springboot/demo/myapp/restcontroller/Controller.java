6f87fa0175ea6d4c4c8e5b4fb40ce45bb15ef2fe
AxelRevolt <depasquale993@gmail.com>
2024-03-19 13:07:36 +0100

    injection value from properties file to code value

--- 01-springboot-overview/06-properties-demo/src/main/java/com/course/springboot/demo/myapp/restcontroller/Controller.java
+++ 01-springboot-overview/06-properties-demo/src/main/java/com/course/springboot/demo/myapp/restcontroller/Controller.java
@@ -1,10 +1,24 @@
 package com.course.springboot.demo.myapp.restcontroller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	
+	// injection properties fro : coach.name and team.name
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	@GetMapping("/propertiestest")
+	public String propertiesTest() {
+		return "CoachName : " + coachName + "\n"
+				+ "Team name : "+ teamName;
+	}
 	
 	@GetMapping("/")
 	public String test() {
