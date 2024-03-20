85aab02bde77d0ae5bf0f2d63f174311c6501bc9
AxelRevolt <depasquale993@gmail.com>
2024-03-20 08:43:29 +0100

    .

--- 02-srpingboot-spring-core/05-qualifiers-primary/src/main/java/com/spring/coredemo/common/Zumba.java
+++ 02-srpingboot-spring-core/05-qualifiers-primary/src/main/java/com/spring/coredemo/common/Zumba.java
@@ -1,7 +1,9 @@
 package com.spring.coredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component // as a spring bean avaible for dependency injection
 public class Zumba implements Coach{
 	
